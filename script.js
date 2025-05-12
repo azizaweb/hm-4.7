3f83f25ba283e1f3b44f8425db120937d7a65834
azizaweb <azizaweb@mail.ru>
2025-05-12 20:29:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded',function(){
+    new Typed("#animated-text", {
+        strings: ["Netflix Splash Screen", ],
+        typeSpeed: 50,
+        backSpeed: 25,
+        startDelay: 500,
+        loop: true
+    });
+})
+document.addEventListener('DOMContentLoaded',function(){
+    new Typed("#imgg", {
+        strings: ["", ],
+        typeSpeed: 50,
+        backSpeed: 25,
+        startDelay: 500,
+        loop: true
+    });
+})
